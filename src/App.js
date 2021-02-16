c4838093f11b1d4d4d9ea116465e0b7cc1c2347e
Cody Sullivan <sullivan.cody7@gmail.com>
2021-02-16 16:24:46 -0700

    Create Tasks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import {useState} from 'react'
-import { FaTrashAlt } from 'react-icons/fa'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1, 
@@ -26,20 +26,27 @@ function App() {
     }
 ])
 
+//Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+} 
+
 //Delete Task
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
   }
 
 //Toggle Reminder
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
   }
 
   return (
     <div className='container'>
-      <Header />
-      <AddTask />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks Available'}
     </div>
 
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,25 @@
 import {useState} from 'react'
 
-const AddTask = () => {
+const AddTask = ({onAdd}) => {
     const [text, setText] = useState('')
     const [day, setDay] = useState('')
     const [reminder, setReminder] = useState(false)
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if(!text) {
+            alert('Please add a task')
+            return
+        }
+
+        onAdd({text, day, reminder})
+        setText('')
+        setDay('')
+        setReminder(false)
+    }
 
     return (
-        <form className='add-form'>
+        <form className='add-form' onSubmit={onSubmit}>
             <div className='form-control'>
                 <label>Task</label>
                 <input type='text' placeholder='Add a Task' value={text} onChange={(e) => setText(e.target.value)}/>
@@ -17,9 +30,9 @@ const AddTask = () => {
             </div>
             <div className='form-control form-control-check'>
                 <label>Set Reminder</label>
-                <input type='Checkbox' />
+                <input type='Checkbox' checked={reminder} value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
             </div>
-            <input type='submit' value='Save Task' className='btn btn-block' value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
+            <input type='submit' value='Save Task' className='btn btn-block' />
         </form>
     )
 }
