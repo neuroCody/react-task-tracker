8f6d39165eea34e68f2d9119d5e9bbe3d1699061
Cody Sullivan <sullivan.cody7@gmail.com>
2021-02-16 14:43:15 -0700

    Delete FUnctions

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import Task from './Task'
 
-const Tasks = () => {
+const Tasks = ({tasks, onDelete, onToggle}) => {
     return (
-        <div>
-            
-        </div>
+        <>
+            {tasks.map((task) =>(
+                <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
+            ))}
+        </>
     )
 }
 
